7e370db80a87ee70bb77c0f34369ee1feba1cf9c
Harza <harzan12345@gmail.com>
2025-06-09 19:16:48 -0400

    Create CalculatorView SwiftUI file.

---
+++ GitSwiftCalculator/CalculatorView.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorView.swift
+//  GitSwiftCalculator
+//
+//  Created by Hojamil on 6/9/25.
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CalculatorView()
+}
