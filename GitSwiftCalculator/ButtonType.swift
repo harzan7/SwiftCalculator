f483d9fe08894dada606ebbdba19da5350c9bddd
Harza <harzan12345@gmail.com>
2025-06-10 19:50:57 -0400

    Add enum ButtonType, with cases for each button that will be on Calculator.

--- GitSwiftCalculator/ButtonType.swift
+++ GitSwiftCalculator/ButtonType.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+enum ButtonType {
+    case zero
+    case one
+    case two
+    case three
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    case addition
+    case subtraction
+    case multiplication
+    case division
+    case negative
+    case percent
+    case decimal
+    case equals
+    case allClear
+    case clear
+}
