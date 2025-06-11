9a5c7295a85976f7751c0f384a6b05d9d774b29e
Harza <harzan12345@gmail.com>
2025-06-10 22:17:51 -0400

    Reduce number of cases. Replace digit cases with digit enum. Replace operation cases with ArithmeticOperation enum.

--- GitSwiftCalculator/Models/ButtonType.swift
+++ GitSwiftCalculator/Models/ButtonType.swift
@@ -8,19 +8,8 @@
 import Foundation
 
 enum ButtonType {
-    case zero
-    case one
-    case two
-    case three
-    case five
-    case six
-    case seven
-    case eight
-    case nine
-    case addition
-    case subtraction
-    case multiplication
-    case division
+    case digit(_ digit: Digit)
+    case operation(_ operation: ArithmeticOperation)
     case negative
     case percent
     case decimal
