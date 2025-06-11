83d6d18aef0c1bdab3212e977900ba6e71007eb2
Harza <harzan12345@gmail.com>
2025-06-10 22:21:24 -0400

    Add description computed property. Gets the button text for a given button on calculator.

--- GitSwiftCalculator/Models/ButtonType.swift
+++ GitSwiftCalculator/Models/ButtonType.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-enum ButtonType {
+enum ButtonType: Hashable, CustomStringConvertible {
     case digit(_ digit: Digit)
     case operation(_ operation: ArithmeticOperation)
     case negative
@@ -16,4 +16,25 @@ enum ButtonType {
     case equals
     case allClear
     case clear
+    
+    var description: String {
+        switch self {
+        case .digit(let digit):
+            return digit.description
+        case .operation(let operation):
+            return operation.description
+        case .negative:
+            return "±"
+        case .percent:
+            return "%"
+        case .decimal:
+            return "."
+        case .equals:
+            return "="
+        case .allClear:
+            return "AC"
+        case .clear:
+            return "C"
+        }
+    }
 }
