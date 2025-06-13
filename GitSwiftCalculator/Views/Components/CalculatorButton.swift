7d041f548dafd93bad213a60eef40b0276f36f05
Harza <harzan12345@gmail.com>
2025-06-12 21:20:10 -0400

    Refactor: Extract button view logic to CalculatorButton.swift.

--- GitSwiftCalculator/Views/Components/CalculatorButton.swift
+++ GitSwiftCalculator/Views/Components/CalculatorButton.swift
@@ -5,4 +5,24 @@
 //  Created by Harza on 6/12/25.
 //
 
-import Foundation
+import SwiftUI
+
+extension CalculatorView {
+    struct CalculatorButton: View {
+        let buttonType: ButtonType
+        
+        var body: some View {
+            Button(buttonType.description) { }
+                .buttonStyle(
+                    CalculatorButtonStyle(
+                        size: 80,
+                        backgroundColor: buttonType.backgroundColor,
+                        foregroundColor: buttonType.foregroundColor))
+        }
+    }
+}
+
+#Preview {
+    CalculatorView.CalculatorButton(
+        buttonType: .digit(.five))
+}
