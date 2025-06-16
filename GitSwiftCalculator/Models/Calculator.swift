3d0a05f10454bb8b0dd252f7fa557a7d9f94aa6b
Harza <harzan12345@gmail.com>
2025-06-16 06:59:38 -0400

    Feat: Display button taps on calculator screen.

--- GitSwiftCalculator/Models/Calculator.swift
+++ GitSwiftCalculator/Models/Calculator.swift
@@ -9,13 +9,31 @@ import Foundation
 
 struct Calculator {
     
+    // MARK: - PROPERTIES
+    private var newNumber: Decimal?
+    
     // MARK: - COMPUTED PROPERTIES
     var displayText: String {
-        return "0"
+        return getNumberString(forNumber: number, withCommas: true)
+    }
+    
+    /// Current displaying number
+    private var number: Decimal? {
+        newNumber
     }
     
     // MARK: - OPERATIONS
     mutating func setDigit(_ digit: Digit) {
+        // 1. Check if you can add digit (01 should not be possible)
+        guard canAddDigit(digit) else { return }
+        
+        // 2. Convert newNumber Decimal to a String
+        let numberString = getNumberString(forNumber: newNumber)
+        
+        // 3. Append digit to end of string,
+        //    convert string back to Decimal,
+        //    assign its new value to newNumber
+        newNumber = Decimal(string: numberString.appending("\(digit.rawValue)"))
         
     }
     
@@ -45,5 +63,14 @@ struct Calculator {
     
     mutating func clear() {
         
+    }
+    
+    // MARK: - HELPERS
+    private func getNumberString(forNumber number: Decimal?, withCommas: Bool = false) -> String {
+        return (withCommas ? number?.formatted(.number) : number.map(String.init)) ?? "0"
+    }
+    
+    private func canAddDigit(_ digit: Digit) -> Bool {
+        return number != nil || digit != .zero
     }
 }
